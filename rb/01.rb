6a82ea678646f24545b5aa1aafe95c4157e83269
Matt Burke <spraints@gmail.com>
2025-12-02 22:10:23 -0500

    two gold stars, also clean up day 1

--- rb/01.rb
+++ rb/01.rb
@@ -1,12 +1,19 @@
 pos = 50
-zeroes = 0
+zeroes1 = 0
+zeroes2 = 0
 ARGF.each_line do |line|
+  #oldpos = pos
   line =~ /(R|L)(\d+)/ or raise line.inspect
   dir = $1
   dist = $2.to_i
   dist *= -1 if dir == "L"
-  pos = (pos + dist) % 100
-  zeroes += 1 if pos == 0
-  #puts "#{line.inspect} => #{pos}"
+  zeroes2 -= 1 if pos == 0 && dir == "L"
+  pos = pos + dist
+  zeroes2 += (pos / 100).abs
+  #p [line, [oldpos, pos], pos / 100, zeroes2]
+  pos %= 100
+  zeroes1 += 1 if pos == 0
+  zeroes2 += 1 if pos == 0 && dir == "L"
+  #puts "#{line.inspect} => #{pos}/#{zeroes1}/#{zeroes2}"
 end
-puts zeroes
+puts zeroes1, zeroes2
