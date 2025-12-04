87fddddbd96d8c5cdc83ff0c7526aafbe4fd350f
Matt Burke <spraints@gmail.com>
2025-12-03 22:05:51 -0500

    six stars ah ah ah

--- rb/03.rb
+++ rb/03.rb
@@ -1,17 +1,36 @@
+def max_j(ary, n, left = 0)
+  if n == 0
+    dbg "=a=> #{n}: #{left}"
+    return left
+  end
+  if ary.size == n
+    tot = ary.inject(left) { |accum, x| accum * 10 + x }
+    dbg "=b=> #{n}: #{tot}"
+  end
+  9.downto(1).each do |x|
+    i = ary.index(x)
+    next if i.nil?
+    dbg "#{x}@#{i}, need #{n-1} more"
+    if tot = max_j(ary[(i+1)..], n - 1, left * 10 + x)
+      dbg "=c=> #{n}(#{x}): #{tot}"
+      return tot
+    end
+  end
+  nil
+end
+
+def dbg(*x)
+  # puts(*x)
+end
+
 banks = ARGF.readlines.map(&:strip).map { |l| l.chars.map(&:to_i) }
 total_joltage = 0
+total_joltage2 = 0
 banks.each do |batteries|
-  #puts batteries.join("")
-  9.downto(1).each do |x|
-    i = batteries.index(x)
-    next if i.nil?
-    rest = batteries[(i+1)..]
-    next if rest.size < 1
-    y = rest.max
-    n = x * 10 + y
-    #puts "==> #{n}"
-    total_joltage += n
-    break
-  end
+  dbg batteries.join("")
+  js = max_j(batteries, 2)
+  total_joltage += js
+  js = max_j(batteries, 12)
+  total_joltage2 += js
 end
-puts total_joltage
+puts total_joltage, total_joltage2
