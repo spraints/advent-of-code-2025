3617736afe4305d8ebd7cb942407469b450e6198
Matt Burke <spraints@gmail.com>
2025-12-04 20:28:35 -0500

    eight stars ah ah ah

--- rb/04.rb
+++ rb/04.rb
@@ -12,12 +12,35 @@ def count_neighbors(grid, i, j)
   res
 end
 
-grid = ARGF.readlines.map { |line| line.strip.chars.to_a }
-ok = 0
-grid.each_with_index do |row, i|
-  row.each_with_index do |cell, j|
-    next if cell != "@"
-    ok += 1 if count_neighbors(grid, i, j) < 4
-  end
+def show_grid(label, grid)
+  #puts "===== #{label} =====", grid.map { |row| row.join("") }
 end
-puts ok
+
+iters = 0
+grid = ARGF.readlines.map { |line| line.strip.chars.to_a }
+show_grid("start", grid)
+removed_by_round = []
+loop do
+  changes = 0
+  new_grid = []
+  grid.each_with_index do |row, i|
+    new_grid << row.each_with_index.map do |cell, j|
+      iters += 1
+      if cell == "x"
+        "."
+      elsif cell == "@" && count_neighbors(grid, i, j) < 4
+        changes += 1
+        "x"
+      else
+        cell
+      end
+    end
+  end
+  show_grid("round #{removed_by_round.size + 1}", new_grid)
+  #puts "===> #{changes}"
+  break if changes == 0
+  removed_by_round << changes
+  grid = new_grid
+end
+puts "(iterations: #{iters})",
+  removed_by_round.first, removed_by_round.sum
