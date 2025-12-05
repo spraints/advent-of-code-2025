ecd15fe55ae7b31a6821cd6a953aa68a0daf2123
Matt Burke <spraints@gmail.com>
2025-12-05 08:54:16 -0500

    nine stars ah ah ah

---
+++ rb/05.rb
@@ -0,0 +1,10 @@
+ranges, ingredients = ARGF.read.split("\n\n", 2)
+ranges = ranges.strip.lines.map { |l| l.strip.split("-",2).map(&:to_i) }
+
+iters = 0
+fresh = 0
+ingredients.strip.lines.each do |ing|
+  ing = ing.strip.to_i
+  fresh += 1 if ranges.any? { |a,b| iters += 1; a <= ing && ing <= b }
+end
+puts "(iterations: #{iters})", fresh
