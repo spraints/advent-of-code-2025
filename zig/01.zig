2b0b424ed7ae2fc28f7e588a68df671df927c54f
Matt Burke <spraints@gmail.com>
2025-12-05 19:34:36 -0500

    Drop an enum

--- zig/01.zig
+++ zig/01.zig
@@ -1,26 +1,21 @@
 const std = @import("std");
 
-const Dir = enum {
-    L,
-    R,
-};
-
 const ClicksRes = struct {
     new_pos: i32,
     clicks: u32,
 };
 
-fn clicks(start: i32, dir: Dir, dist: i32) ClicksRes {
+fn clicks(start: i32, dir: u8, dist: i32) ClicksRes {
     // std.debug.print("clicks {} {} {}\n", .{start,dir,dist});
     if (dist == 0) {
         return .{.new_pos = start, .clicks = 0};
     }
-    const off = if (dir == .L) -dist else dist;
+    const off = if (dir == 'L') -dist else dist;
     const new = start + off;
     const new_real = @mod(new, 100);
     const q = if (new > 0) @divTrunc(new, 100) else @divTrunc(new-100, 100);
     var ct: u32 = @abs(q);
-    if (start == 0 and dir == .L) {
+    if (start == 0 and dir == 'L') {
         ct -= 1;
     }
     // std.debug.print("{} => {}({}) ~~~ {}/{}\n", .{start,new_real,new,q,ct});
@@ -28,26 +23,26 @@ fn clicks(start: i32, dir: Dir, dist: i32) ClicksRes {
 }
 
 test clicks {
-    try std.testing.expectEqual(ClicksRes{.new_pos = 50, .clicks =0}, clicks(50,.L,0));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =1}, clicks(50,.L,50));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =1}, clicks(50,.L,51));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =2}, clicks(50,.L,151));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 50, .clicks =0}, clicks(50,'L',0));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =1}, clicks(50,'L',50));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =1}, clicks(50,'L',51));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =2}, clicks(50,'L',151));
 
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =0}, clicks(0,.L,0));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =0}, clicks(0,.L,1));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 1, .clicks =0}, clicks(0,.L,99));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =1}, clicks(0,.L,100));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =1}, clicks(0,.L,101));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =2}, clicks(0,.L,201));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =0}, clicks(0,'L',0));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =0}, clicks(0,'L',1));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 1, .clicks =0}, clicks(0,'L',99));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks =1}, clicks(0,'L',100));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =1}, clicks(0,'L',101));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks =2}, clicks(0,'L',201));
 
-    try std.testing.expectEqual(ClicksRes{.new_pos = 50, .clicks = 0}, clicks(50,.R,0));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks = 0}, clicks(50,.R,49));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 1}, clicks(50,.R,50));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 2}, clicks(50,.R,150));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 50, .clicks = 0}, clicks(50,'R',0));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks = 0}, clicks(50,'R',49));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 1}, clicks(50,'R',50));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 2}, clicks(50,'R',150));
 
-    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks = 0}, clicks(0,.R,99));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 1}, clicks(0,.R,100));
-    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 2}, clicks(0,.R,200));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 99, .clicks = 0}, clicks(0,'R',99));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 1}, clicks(0,'R',100));
+    try std.testing.expectEqual(ClicksRes{.new_pos = 0, .clicks = 2}, clicks(0,'R',200));
 }
 
 pub fn main() !void {
