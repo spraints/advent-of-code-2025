b490445d137cb538b24abada95c395e7453e68db
Matt Burke <spraints@gmail.com>
2025-12-06 18:33:35 -0500

    this is wrong

--- rb/06.rb
+++ rb/06.rb
@@ -1,15 +1,20 @@
 homework = ARGF.readlines.map { |line| line.strip.split }
-total = 0
+part1 = 0
+part2 = 0
 ops = homework.pop
-homework = homework.map { |l| l.map(&:to_i) }
 ops.each_with_index do |op, i|
+  ns = homework.map { |l| l[i] }
+  p1 = ns.map(&:to_i)
+  p2 = ns.map(&:reverse).map(&:to_i)
   case op
   when "+"
-    total += homework.map { |l| l[i] }.sum
+    part1 += p1.inject(&:+)
+    part2 += p2.inject(&:+)
   when "*"
-    total += homework.map { |l| l[i] }.inject(&:*)
+    part1 += p1.inject(&:*)
+    part2 += p2.inject(&:*)
   else
     raise "invalid op[#{i}] #{op.inspect}"
   end
 end
-puts total
+puts part1, part2
