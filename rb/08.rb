11f3ea6ed66944e2c2a81d3d8698b14c5f3462a9
Matt Burke <spraints@gmail.com>
2025-12-08 21:48:37 -0500

    another star ah ah ah

---
+++ rb/08.rb
@@ -0,0 +1,51 @@
+points = ARGF.readlines.map { |line| line.strip.split(",").map(&:to_i) }
+goal =
+  case points.size
+  when 20
+    # sample
+    10
+  when 1000
+    # real
+    1000
+  else
+    abort "unknown goal"
+  end
+
+def dist(a, b)
+  # d1 = sqrt( (bx - ax)^2 + (by - ay)^2 )
+  # d2 = sqrt( d1^2 + (bz - az)^2 )
+  # d2 = sqrt( (bx - ax)^2 + (by - ay)^2 + (bz - az)^2 )
+  raise "invalid #{a.size} vs #{b.size}" if a.size != b.size
+  squares = a.zip(b).map { |aa, bb| (aa.to_f - bb.to_f)**2 }
+  Math.sqrt(squares.sum)
+end
+
+dists = []
+circuits = {}
+larger_than_1 = []
+points.each_with_index do |p1, i|
+  circuits[p1] = [p1]
+  points[i+1..].each_with_index do |p2, j|
+    dists << [dist(p1, p2), p1, p2]
+  end
+end
+dists.sort.take(goal).each do |_, p1, p2|
+  c1 = circuits.fetch(p1)
+  c2 = circuits.fetch(p2)
+  if c1 == c2
+    #puts "redundant"
+    next
+  end
+  #require "pp"; pp merge: [c1, c2]
+  combined = c1 + c2
+  combined.each do |p|
+    circuits[p] = combined
+  end
+  larger_than_1.delete(c1)
+  larger_than_1.delete(c2)
+  larger_than_1 << combined
+end
+
+#require "pp"; pp larger_than_1
+
+puts larger_than_1.map(&:size).sort_by { |x| -x }.take(3).inject(&:*)
