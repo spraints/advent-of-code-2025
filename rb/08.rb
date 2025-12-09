475ba0ad5648fa447b1dc425617a3aa0bb57c3be
Matt Burke <spraints@gmail.com>
2025-12-08 21:51:42 -0500

    sixteen stars ah ah ah

--- rb/08.rb
+++ rb/08.rb
@@ -22,20 +22,29 @@ end
 
 dists = []
 circuits = {}
+loners = Set.new
 larger_than_1 = []
 points.each_with_index do |p1, i|
   circuits[p1] = [p1]
+  loners << p1
   points[i+1..].each_with_index do |p2, j|
     dists << [dist(p1, p2), p1, p2]
   end
 end
-dists.sort.take(goal).each do |_, p1, p2|
+dists.sort!
+(0..goal-1).each do |i|
+  dist, p1, p2 = dists[i]
   c1 = circuits.fetch(p1)
+  loners.delete(p1)
   c2 = circuits.fetch(p2)
+  loners.delete(p2)
   if c1 == c2
     #puts "redundant"
     next
   end
+  #p p1: p1, p2: p2, dist: dist
+  #p c1: c1, c2: c2
+  #p loners: loners.to_a.sort
   #require "pp"; pp merge: [c1, c2]
   combined = c1 + c2
   combined.each do |p|
@@ -49,3 +58,36 @@ end
 #require "pp"; pp larger_than_1
 
 puts larger_than_1.map(&:size).sort_by { |x| -x }.take(3).inject(&:*)
+
+(goal..).each do |i|
+  dist, p1, p2 = dists[i]
+
+  c1 = circuits.fetch(p1)
+  loners.delete(p1)
+
+  c2 = circuits.fetch(p2)
+  loners.delete(p2)
+
+  if c1 == c2
+    #puts "redundant"
+    next
+  end
+
+  #p p1: p1, p2: p2, dist: dist
+  #p c1: c1, c2: c2
+  #p loners: loners.to_a.sort, globs: larger_than_1.map(&:size)
+
+  larger_than_1.delete(c1)
+  larger_than_1.delete(c2)
+  if loners.empty? && larger_than_1.empty?
+    puts p1[0] * p2[0]
+    break
+  end
+
+  #require "pp"; pp merge: [c1, c2]
+  combined = c1 + c2
+  combined.each do |p|
+    circuits[p] = combined
+  end
+  larger_than_1 << combined
+end
