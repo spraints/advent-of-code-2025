4f6ace6c82bbb13565d9998770fa562df3ad10f3
Matt Burke <spraints@planetscale.com>
2025-12-11 10:47:27 -0500

    sample part 2 works

--- rb/09.rb
+++ rb/09.rb
@@ -1,3 +1,5 @@
+DEBUG = !ENV["DEBUG"].nil?
+
 class Coord
   def initialize(pt)
     @x = pt[0]
@@ -10,11 +12,28 @@ class Coord
   def inspect
     [self.x, self.y].inspect
   end
+  def ==(other)
+    self.x == other.x && self.y == other.y
+  end
+  def eql?(other)
+    self.x == other.x && self.y == other.y
+  end
+  def hash
+    [self.x, self.y].hash
+  end
 end
+
+tpt1 = Coord.new([1,1])
+tpt2 = Coord.new([1,1])
+tpt3 = Coord.new([1,2])
+tpt4 = Coord.new([2,1])
+raise "boom" if tpt1 != tpt2
+raise "boom" if tpt1 == tpt3
+raise "boom" if tpt1 == tpt4
 
 coords = ARGF.readlines.map { |line| Coord.new(line.strip.split(",").map(&:to_i)) }
 
-if ENV["PIC"]
+if DEBUG
   picture = []
   last_pt = coords.last
   coords.each do |pt|
@@ -53,10 +72,35 @@ def area(p1, p2)
   l * w
 end
 
+def vert_edge_dir(edge, pt, verbose)
+  ep1, ep2 = edge
+  case
+  when ep1.y < pt.y && pt.y < ep2.y
+    puts "from #{pt} to #{edge}: cross" if verbose
+    :cross
+  when ep1.y == pt.y && ep2.y < pt.y
+    puts "from #{pt} to #{edge}: up1" if verbose
+    :up
+  when ep2.y == pt.y && ep1.y < pt.y
+    puts "from #{pt} to #{edge}: up2" if verbose
+    :up
+  when ep1.y == pt.y && ep2.y > pt.y
+    puts "from #{pt} to #{edge}: down1" if verbose
+    :down
+  when ep2.y == pt.y && ep1.y > pt.y
+    puts "from #{pt} to #{edge}: down2" if verbose
+    :down
+  else
+    puts "from #{pt} to #{edge}: ???" if verbose
+    :unknown
+  end
+end
+
 class Bounds
   def initialize
     @vertical_edges = []
     @horizontal_edges = []
+    @cache = {}
   end
   def add_edge(p1, p2)
     edge = [p1, p2].sort
@@ -71,21 +115,41 @@ class Bounds
     nil
   end
   def contains?(pt)
-    n = count_edges(pt)
+    if n = @cache[pt]
+      p cached: pt, n: n if DEBUG
+    else
+      @cache[pt] = n = count_edges(pt)
+    end
     n % 2 == 1
   end
   private
   def count_edges(pt)
+    verbose = DEBUG && pt.x == 9 && pt.y == 5
     vert_matches = @vertical_edges.select { |e|
       e1, e2 = e
-      #p check: e, px: pt.x, py: pt.y,
-      #  xok: e1.x == pt.x && e1.x == pt.x,
-      #  yok: e1.y <= pt.y && pt.y <= e2.y
-      e1.x == pt.x && e1.x == pt.x && e1.y <= pt.y && pt.y <= e2.y
+      xok = e1.x <= pt.x
+      yok = e1.y <= pt.y && pt.y <= e2.y
+      if verbose
+        p check: e, px: pt.x, py: pt.y, xok: xok, yok: yok
+      end
+      xok && yok
     }
+    dropped = 0
+    vert_matches.combination(2).each do |e1, e2|
+      p e1: e1, e2: e2 if verbose
+      dir1 = vert_edge_dir(e1, pt, verbose)
+      dir2 = vert_edge_dir(e2, pt, verbose)
+      next unless [dir1, dir2].sort == [:down, :up]
+      goal = [Coord.new([e1[0].x, pt.y]), Coord.new([e2[0].x, pt.y])].sort
+      puts "checking if there is an edge between #{e1} and #{e2} (#{goal})..." \
+        if verbose
+      next unless @horizontal_edges.include?(goal)
+      puts "... there is!" if verbose
+      dropped += 1
+    end
 
-    n = vert_matches.size
-    pp summary_for: pt, n: n, vert: vert_matches
+    n = vert_matches.size - dropped
+    pp summary_for: pt, n: n, vert: vert_matches if DEBUG
     n
   end
   require "pp"
@@ -102,7 +166,7 @@ coords.each_with_index do |p1, i|
   rest = coords[i+2..] or next
   rest.each do |p2|
     a = area(p1, p2)
-    p check_rect: [p1, p2], a: a
+    p check_rect: [p1, p2], a: a if DEBUG
     max_area1 = [max_area1, a].max
     if a > max_area2
       # Check if all 4 corners are inside the shape.
@@ -112,7 +176,7 @@ coords.each_with_index do |p1, i|
         end
       end
       if contained
-        puts "========> update part 2"
+        puts "========> update part 2 = #{a}" if DEBUG
         max_area2 = a
       end
     end
