0410d9f3e84ea05344a71ed78098ff3aefbc90a2
Matt Burke <spraints@gmail.com>
2025-12-17 21:54:08 -0500

    twenty ah ah ah

--- rb/11.rb
+++ rb/11.rb
@@ -1,7 +1,14 @@
 require "set"
 
 paths = {}
+want = %w[you svr dac fft]
+has = {}
 ARGF.each_line do |line|
+  want.each do |w|
+    if line =~ /#{w}/
+      has[w] = true
+    end
+  end
   src, dsts = line.split(":", 2)
   paths[src] = dsts.strip.split
 end
@@ -20,4 +27,28 @@ def count_paths(paths, src, memo = {})
   memo[src] = res
 end
 
-puts count_paths(paths, "you")
+puts "part 1: #{count_paths(paths, "you")}" if has["you"]
+
+def count_paths2(paths, src, memo: {}, fft: false, dac: false, trace: [])
+  # p cur: src, fft: false, dac: false, trace: trace
+  k = [src, fft, dac]
+  return memo[k] if memo[k]
+  res = 0
+  paths[src].each do |n|
+    t2 = trace + [n]
+    case n
+    when "out"
+      # p t2
+      res += 1 if fft && dac
+    when "fft"
+      res += count_paths2(paths, n, memo: memo, fft: true, dac: dac, trace: t2)
+    when "dac"
+      res += count_paths2(paths, n, memo: memo, fft: fft, dac: true, trace: t2)
+    else
+      res += count_paths2(paths, n, memo: memo, fft: fft, dac: dac, trace: t2)
+    end
+  end
+  memo[k] = res
+end
+
+puts "part 2: #{count_paths2(paths, "svr")}" if has["svr"] && has["fft"] && has["dac"]
